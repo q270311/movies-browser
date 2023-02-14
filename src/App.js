6d73f589c997587e903c42959fd1bdc958d74835
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-14 19:04:04 +0100

    unnecessary content deleted

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Movies Browser
-        </p>
-        <p>Site under construction</p>
-      </header>
+    <div>
     </div>
   );
 }
