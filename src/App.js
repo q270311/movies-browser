a8069a3655c267993898453829287342563c5790
Michał Bałdyga <q270311@gmail.com>
2023-02-15 13:08:49 +0100

    deleted global style from  App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import { GlobalStyle } from './GlobalStyle';
 
 function App() {
   return (
-    <>
-      <GlobalStyle />
       <div>
         <p>Movies Browser</p>
         <p>site under construction</p>
       </div>
-    </>
   );
 }
 
