8de920afea81206936cca3c18e085e2e662f07d2
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-15 21:05:22 +0100

    Genres, Genre styles added

--- src/common/MovieTile/styled.js
+++ src/common/MovieTile/styled.js
@@ -36,11 +36,23 @@ export const Year = styled.p`
 `;
 
 export const Genres = styled.div`
-
+  display: flex;
+  flex-wrap: wrap;
+  padding: 0px;
+  gap: 8px;
 `;
 
 export const Genre = styled.p`
+  background-color: #E4E6F0;
+  border-radius: 5px;
+  margin: 0;
+  padding: 8px 16px;
+  font-size: 14px;
 
+  @media (max-width: 600px) {
+    align-self: flex-start;
+    font-size: 13px;
+  }
 `;
 
 export const Votes = styled.div`
