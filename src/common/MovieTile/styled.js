2b307ecd2325d879b9eaec5f80f25a4906637c98
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-15 22:51:48 +0100

    Title font-weight, margin-bottom added

--- src/common/MovieTile/styled.js
+++ src/common/MovieTile/styled.js
@@ -35,10 +35,12 @@ export const Title = styled.h2`
   font-size: 22px;
   margin-top: 16px;
   margin-bottom: 8px;
-
+  font-weight: 500;
+  
   @media (max-width: 400px) {
     font-size: 16px;
     margin-top: 0;
+    margin-bottom: 4px;
 `;
 
 export const Year = styled.p`
