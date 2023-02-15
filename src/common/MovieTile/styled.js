505558e0a286a9bbcd9102e7edef7eb0d2fa2700
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-15 20:37:43 +0100

    Tile styles added

--- src/common/MovieTile/styled.js
+++ src/common/MovieTile/styled.js
@@ -1,7 +1,14 @@
 import styled from "styled-components";
 
 export const Tile = styled.div`
-
+  display:flex;
+  flex-direction: column;
+  background-color: white;
+  box-shadow: 0px 4px 12px rgba(186, 199, 213, 0.5);
+  margin: 16px;
+  min-width: 324px;
+  height: auto;
+  border-radius: 5px;
 `;
 
 export const ImageWrapper = styled.div`
