2dad919f5fd1a49024a1304aa5c55976440d3fc2
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-15 20:06:32 +0100

    MovieTile structure added

---
+++ src/common/MovieTile/index.js
@@ -0,0 +1,26 @@
+import { Tile, ImageWrapper, Image, Description, Title, Year, Genres, Genre, Votes, Vote } from "./styled";
+import poster from "./poster.png"
+
+export const MovieTile = () => (
+  <>
+    <Tile>
+      <ImageWrapper>
+        <Image alt="moviePoster" src={poster} />
+      </ImageWrapper>
+      <Description>
+          <Title>Mulan</Title>
+          <Year>2020</Year>
+          <Genres>
+            <Genre>Action</Genre>
+            <Genre>Adventure</Genre>
+            <Genre>Drama</Genre>
+          </Genres>
+          <Votes>
+            <Vote star><image class="star" src="./star.svg" alt="" /></Vote>
+            <Vote>7,7</Vote>
+            <Vote amount>35 votes</Vote>
+          </Votes>
+      </Description>
+    </Tile>
+  </>
+);
---
+++ src/common/MovieTile/styled.js
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+
+export const Tile = styled.div`
+
+`;
+
+export const ImageWrapper = styled.div`
+
+`;
+
+export const Image = styled.img`
+
+`;
+
+export const Description = styled.div`
+
+`;
+
+export const Title = styled.h2`
+
+`;
+
+export const Year = styled.p`
+
+`;
+
+export const Genres = styled.div`
+
+`;
+
+export const Genre = styled.p`
+
+`;
+
+export const Votes = styled.div`
+
+`;
+
+export const Vote = styled.p`
+
+`;
