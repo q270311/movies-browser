9ffb208a371103b705fbb76913f0252aaa60c7fb
Joanna Panimasz <pani.joanna93@gmail.com>
2023-02-15 22:56:34 +0100

    add and import Provider in index.js

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import reportWebVitals from "./reportWebVitals";
 import { GlobalStyle } from "./GlobalStyle";
 import { theme } from "./theme";
 import { ThemeProvider } from "styled-components";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <Provider>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
 
