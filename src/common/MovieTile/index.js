6239bda1cbf3224a3d90376b5bf7f79b61acd8ab
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-15 20:48:45 +0100

    ImageWrapper deleted

--- src/common/MovieTile/index.js
+++ src/common/MovieTile/index.js
@@ -4,22 +4,20 @@ import poster from "./poster.png"
 export const MovieTile = () => (
   <>
     <Tile>
-      <ImageWrapper>
-        <Image alt="moviePoster" src={poster} />
-      </ImageWrapper>
+      <Image alt="moviePoster" src={poster} />
       <Description>
-          <Title>Mulan</Title>
-          <Year>2020</Year>
-          <Genres>
-            <Genre>Action</Genre>
-            <Genre>Adventure</Genre>
-            <Genre>Drama</Genre>
-          </Genres>
-          <Votes>
-            <Vote star><image class="star" src="./star.svg" alt="" /></Vote>
-            <Vote>7,7</Vote>
-            <Vote amount>35 votes</Vote>
-          </Votes>
+        <Title>Mulan</Title>
+        <Year>2020</Year>
+        <Genres>
+          <Genre>Action</Genre>
+          <Genre>Adventure</Genre>
+          <Genre>Drama</Genre>
+        </Genres>
+        <Votes>
+          <Vote star><img className="star" src="./star.svg" alt="" /></Vote>
+          <Vote>7,7</Vote>
+          <Vote amount>35 votes</Vote>
+        </Votes>
       </Description>
     </Tile>
   </>
