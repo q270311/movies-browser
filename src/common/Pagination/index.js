4e683093a0fb1769783c448ef984279ff869e0f2
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-16 19:53:55 +0100

    StyledVector added

--- src/common/Pagination/index.js
+++ src/common/Pagination/index.js
@@ -1,19 +1,27 @@
-import { Wrapper, Button } from "./styled";
+import { Wrapper, Button, StyledVector } from "./styled";
 
 export const Pagination = () => {
   return (
     <Wrapper>
       <Button>
+        <StyledVector/>
+        <StyledVector/>
         First
       </Button>
       <Button>
+      <StyledVector/>
+      <StyledVector/>
         Previous
       </Button>
       <p></p>
       <Button>
+      <StyledVector/>
+      <StyledVector/>
         Next
       </Button>
       <Button>
+      <StyledVector/>
+      <StyledVector/>
         Last
       </Button>
     </Wrapper>
