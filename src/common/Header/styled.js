ae20513672d11a0700157ea5c77dc7c765240b03
Joanna Panimasz <pani.joanna93@gmail.com>
2023-02-16 21:59:01 +0100

    add styles to the styled.js in Header

--- src/common/Header/styled.js
+++ src/common/Header/styled.js
@@ -5,3 +5,23 @@ width: 1920px;
 height: 94px;
 background: ${({ theme }) => theme.color.black};
 `;
+
+export const Container = styled.div`
+`;
+
+export const Video = styled.img`
+
+`;
+
+export const Title = styled.h1`
+width: 168px;
+height: 40px;
+font-family: 'Poppins';
+font-style: normal;
+font-weight: 500;
+font-size: 24px;
+line-height: 40px;
+letter-spacing: -1.5px;
+text-transform: capitalize;
+color: #FFFFFF;
+`;
