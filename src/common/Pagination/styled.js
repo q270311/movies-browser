2f1117e511a45b4cca45a945175a062a30ac001f
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-16 18:59:59 +0100

    Button styles added

--- src/common/Pagination/styled.js
+++ src/common/Pagination/styled.js
@@ -2,8 +2,13 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
   display: inline-flex;
+  gap: 8px;
 `;
 
 export const Button = styled.button`
   padding: 8px 16px;
+  font-size: 14px;
+  background-color: #D6E4FF;
+  border-radius: 5px;
+  border: none;
 `;
