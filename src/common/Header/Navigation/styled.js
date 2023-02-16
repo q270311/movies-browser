6f92979e38009fdc787fca5d8c72db1745d35997
Joanna Panimasz <pani.joanna93@gmail.com>
2023-02-16 22:47:57 +0100

    add flexbox to the NavList

--- src/common/Header/Navigation/styled.js
+++ src/common/Header/Navigation/styled.js
@@ -3,17 +3,18 @@ import styled from "styled-components";
 export const StyledNavigation = styled.nav``;
 
 export const NavList = styled.ul`
+  display: flex;
   list-style-type: none;
 `;
 
 export const NavItems = styled.li`
-width: 54px;
-height: 21px;
-font-family: 'Poppins';
-font-style: normal;
-font-weight: 600;
-font-size: 14px;
-line-height: 21px;
-text-transform: uppercase;
-color: ${({ theme }) => theme.color.white};
+  width: 54px;
+  height: 21px;
+  font-family: "Poppins";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 21px;
+  text-transform: uppercase;
+  color: ${({ theme }) => theme.color.white};
 `;
