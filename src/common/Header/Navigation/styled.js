c774e91c386fd3d29551a1a7fdf07b9707ba0ec5
Joanna Panimasz <pani.joanna93@gmail.com>
2023-02-16 22:56:33 +0100

    add styles to the NavList

--- src/common/Header/Navigation/styled.js
+++ src/common/Header/Navigation/styled.js
@@ -4,7 +4,14 @@ export const StyledNavigation = styled.nav``;
 
 export const NavList = styled.ul`
   display: flex;
+  align-items: center;
   list-style-type: none;
+  padding: 8px 24px;
+  gap: 10px;
+  width: 102px;
+  height: 48px;
+  border: 1px solid #ffffff;
+  border-radius: 24px;
 `;
 
 export const NavItems = styled.li`
