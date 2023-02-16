c0428984de717f3e01eb1ecbb342bc88f96b6866
Joanna Panimasz <pani.joanna93@gmail.com>
2023-02-16 22:20:06 +0100

    add NavigationLink to the styled.js

--- src/common/Header/styled.js
+++ src/common/Header/styled.js
@@ -1,33 +1,39 @@
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 export const StyledHeader = styled.header`
-display: flex;
-width: 1920px;
-height: 94px;
-background: ${({ theme }) => theme.color.black};
+  display: flex;
+  width: 1920px;
+  height: 94px;
+  background: ${({ theme }) => theme.color.black};
 `;
 
 export const Container = styled.div`
-display: flex;
-align-items: center;
-gap: 16px;
+  display: flex;
+  align-items: center;
 `;
 
 export const Video = styled.img`
-width: 40px;
-height: 40px;
+  width: 40px;
+  height: 40px;
 `;
 
 export const Title = styled.h1`
-width: 168px;
-height: 40px;
-font-family: 'Poppins';
-font-style: normal;
-font-weight: 500;
-font-size: 24px;
-line-height: 40px;
-letter-spacing: -1.5px;
-text-transform: capitalize;
-color: ${({ theme }) => theme.color.white};
-margin: 0;
+  width: 168px;
+  height: 40px;
+  font-family: "Poppins";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 40px;
+  letter-spacing: -1.5px;
+  text-transform: capitalize;
+  color: ${({ theme }) => theme.color.white};
+  margin: 0;
+`;
+
+export const NavigationLink = styled(NavLink)`
+display: flex;
+  text-decoration: none;
+  gap: 16px;
 `;
