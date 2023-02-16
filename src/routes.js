b0a0226bbbb19f118c35bb9c056a2eb1760d2bfd
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-16 13:34:52 +0100

    Routes initial commit

---
+++ src/routes.js
@@ -0,0 +1,4 @@
+export const toMovieDetails = () => "/movie-details";
+export const toPersonDetails = () => "/person-details";
+export const toMoviesList = () => "/movies-list";
+export const toPeopleList = () => "/people-list";
