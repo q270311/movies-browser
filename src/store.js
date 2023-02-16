915935df2af1e25b89a270f655e1a9fb9769bf27
Michał Bałdyga <q270311@gmail.com>
2023-02-16 15:29:01 +0100

    rootSaga added to stor

--- src/store.js
+++ src/store.js
@@ -1,8 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
+import createSagaMiddleware from "redux-saga";
+import rootSaga from "./rootSaga";
+
+const sagaMiddleware= createSagaMiddleware();
 
 const store = configureStore({
     reducer: {
     },
+    middleware: [sagaMiddleware],
 });
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
