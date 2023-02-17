4d987cca444d9b13d7e183f1c3c3727d3095d803
Michał Bałdyga <q270311@gmail.com>
2023-02-17 18:43:20 +0100

    watchFetchMoviePopular added to root Saga

---
+++ src/features/popularMovies/popularMovieSaga.js
@@ -0,0 +1,16 @@
+import { call, put } from "redux-saga/effects";
+import { fetchPopularMovieSuccess, fetchPopularMovieError } from "./popularMovieSlice";
+import { getMoviePopular } from "../../getDataFromMovieDatabaseAPI";
+
+function* fetchMoviePopularHandler() {
+    try {
+        const movies = yield call(getMoviePopular);
+        yield put(fetchPopularMovieSuccess(movies.results));
+    } catch (error) {
+        yield put(fetchPopularMovieError());
+    }
+}
+
+export function* watchFetchMoviePopular() {
+    yield fetchMoviePopularHandler();
+}
