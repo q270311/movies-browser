08a786a54cdbb059c465e57d4424ec11f68c95ad
Joanna Panimasz <pani.joanna93@gmail.com>
2023-02-17 15:06:52 +0100

    add flexbox property to StyleHeader

--- src/common/Header/styled.js
+++ src/common/Header/styled.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 export const StyledHeader = styled.header`
   display: flex;
   justify-content: space-between;
+  align-items: center;
   padding: 16px;
   width: 100%;
   height: 94px;
