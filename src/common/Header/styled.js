a4ddf95fb95a321721df5077fd217439bc61209c
Joanna Panimasz <pani.joanna93@gmail.com>
2023-02-17 15:00:59 +0100

    remove justify-content from Container

--- src/common/Header/styled.js
+++ src/common/Header/styled.js
@@ -13,7 +13,6 @@ export const StyledHeader = styled.header`
 export const Container = styled.div`
   display: flex;
   align-items: center;
-  justify-content: space-between;
 `;
 
 export const Video = styled.img`
