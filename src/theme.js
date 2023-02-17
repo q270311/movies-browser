52ddc4b65fb166dc01f69685a15261374f8af650
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-17 18:37:33 +0100

    pagination colors added

--- src/theme.js
+++ src/theme.js
@@ -3,6 +3,12 @@ export const theme = {
     black: "#18181B",
     white: "#FFFFFF",
     grey: "#7E839A",
+    pattensBlue: "#D6E4FF",
+    mine: "#333333",
+    mystic: "#E4E6F0",
+    woodsmoke: "#18181B",
+    blue: "#0044CC",
+    waterloo: "#7E839A"
   },
   breakpoint: {
     mobileDevice: 767,
