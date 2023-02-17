90190cd2a7134b46e63aa0cc95eecab7c8791baf
Joanna Panimasz <pani.joanna93@gmail.com>
2023-02-17 12:03:48 +0100

    add styles to the SearchInput

--- src/common/Header/Search/styled.js
+++ src/common/Header/Search/styled.js
@@ -1,6 +1,19 @@
 import styled from "styled-components";
 
-export const SearchInput = styled.input``;
+export const SearchInput = styled.input`
+  font-family: "Poppins";
+  font-style: normal;
+  color: ${({ theme }) => theme.color.grey};
+  border: none;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 1.5;
+  background-color: transparent;
+  outline: 0;
+  padding: 0;
+  width: 155px;
+  height: 24px;
+`;
 
 export const SearchIcon = styled.img`
   width: 17.98px;
