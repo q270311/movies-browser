a4c788d022ed6e60ca496050ac6acdf49c48cd4b
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-18 14:08:21 +0100

    Genres added

---
+++ src/common/MovieTile/Genres/styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Genres = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  padding: 0px;
+  gap: 8px;
+  margin-top: 8px;
+  margin-bottom: 8px;
+`;
