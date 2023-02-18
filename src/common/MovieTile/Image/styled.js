19cb5351aa35bc9c1c369cc4d50cfa6cfad81a2d
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-18 20:39:49 +0100

    align-items added

--- src/common/MovieTile/Image/styled.js
+++ src/common/MovieTile/Image/styled.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const ImageWrapper = styled.div`
   display: grid;
   grid-row: 1/3;
+  align-items: center;
 
   @media (max-width: ${({ theme }) => theme.breakpoint.mobileDevice}px) {
     margin-right: 16px;
