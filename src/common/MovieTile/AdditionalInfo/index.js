79be622376a6dc9551027f6bd05e90a1d94f5843
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-19 00:36:16 +0100

    Value added

--- src/common/MovieTile/AdditionalInfo/index.js
+++ src/common/MovieTile/AdditionalInfo/index.js
@@ -4,9 +4,11 @@ export const AdditionalInfo = () => (
   <Wrapper>
     <Paragraph>
       <Attribute>Production:&nbsp;</Attribute>
+      <Value>China USA</Value>
     </Paragraph>
     <Paragraph>
       <Attribute>Release date:&nbsp;</Attribute>
+      <Value>24.10.2022</Value>
     </Paragraph>
   </Wrapper>
 );
