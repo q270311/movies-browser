9f7222c3c739805c14162c58054afea7e6d79338
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-19 00:34:57 +0100

    ExtendedVotes added

--- src/common/MovieTile/Votes/index.js
+++ src/common/MovieTile/Votes/index.js
@@ -1,4 +1,4 @@
-import { Wrapper, Vote, Star } from "./styled";
+import { Wrapper, Vote, Star, Total } from "./styled";
 import star from "./star.svg";
 
 export const Votes = () => (
@@ -16,6 +16,31 @@ export const Votes = () => (
     </Vote>
     <Vote
       count
+    >
+      35 votes
+    </Vote>
+  </Wrapper>
+);
+
+export const ExtendedVotes = () => (
+  <Wrapper>
+    <Vote>
+      <Star
+        src={star}
+        alt=""
+      />
+    </Vote>
+    <Vote
+      average
+      extended
+    >
+      7,7
+    </Vote>
+    <Total>
+      /10
+    </Total>
+    <Vote
+      smaller
     >
       35 votes
     </Vote>
