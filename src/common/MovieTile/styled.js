ef64e65e176b0453199134b69d0cbbb268421896
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-18 14:27:06 +0100

    Title, Year, css deleted

--- src/common/MovieTile/styled.js
+++ src/common/MovieTile/styled.js
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
 export const Tile = styled.div`
   display: flex;
@@ -26,22 +26,4 @@ export const Description = styled.div`
   @media (max-width: ${({ theme }) => theme.breakpoint.mobileDevice}px) {
     height: auto;
   }
-`;
-
-export const Title = styled.h2`
-  font-size: 22px;
-  margin-top: 16px;
-  margin-bottom: 8px;
-  font-weight: 500;
-  word-break: break-word;
-  
-  @media (max-width: ${({ theme }) => theme.breakpoint.mobileDevice}px) {
-    font-size: 16px;
-    margin-top: 0;
-    margin-bottom: 4px;
-`;
-
-export const Year = styled.p`
-  color: ${({ theme }) => theme.color.waterloo};
-  margin: 0;
 `;
