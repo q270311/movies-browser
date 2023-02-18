5c74231df1d3d7eafadd50c3c2c3fd6175575465
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-18 14:16:43 +0100

    Vote, Star deleted

--- src/common/MovieTile/index.js
+++ src/common/MovieTile/index.js
@@ -1,6 +1,5 @@
 import { Tile, Description, Title, Year, Votes, Vote, Star } from "./styled";
 import { Image } from "./Image";
-import star from "./star.svg";
 import { Genres } from "./Genres";
 
 export const MovieTile = () => (
@@ -15,24 +14,7 @@ export const MovieTile = () => (
           2020
         </Year>
         <Genres />
-        <Votes>
-          <Vote>
-            <Star
-              src={star}
-              alt=""
-            />
-          </Vote>
-          <Vote
-            average
-          >
-            7,7
-          </Vote>
-          <Vote
-            count
-          >
-            35 votes
-          </Vote>
-        </Votes>
+        <Votes />
       </Description>
     </Tile>
   </>
