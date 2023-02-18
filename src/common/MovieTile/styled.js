d7406b81fbda562b035e882e786cfa407edbac72
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-18 14:14:02 +0100

    Votes, Vote deleted

--- src/common/MovieTile/styled.js
+++ src/common/MovieTile/styled.js
@@ -46,31 +46,6 @@ export const Year = styled.p`
   margin: 0;
 `;
 
-export const Votes = styled.div`
-  display: flex;
-  height: 100%;
-  gap: 12px;
-`;
-
-export const Vote = styled.p`
-  margin: 0;
-  display: flex;
-  align-self: flex-end;
-
-  @media (max-width: ${({ theme }) => theme.breakpoint.mobileDevice}px) {
-    font-size: 13px;
-    align-self: flex-start;
-  }
-  
-  ${({ average }) => average && css`
-    font-weight: 600;
-  `}
-
-  ${({ count }) => count && css`
-    color: ${({ theme }) => theme.color.waterloo};
-  `}
-`;
-
 export const Star = styled.img`
   width: 24px;
 
