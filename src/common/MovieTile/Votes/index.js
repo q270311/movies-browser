2e930caccdc88b8258def3920c78faa65e51da41
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-18 14:16:59 +0100

    Wrapper, Vote added

---
+++ src/common/MovieTile/Votes/index.js
@@ -0,0 +1,23 @@
+import { Wrapper, Vote } from "./styled";
+import star from "./star.svg";
+
+export const Votes = () => (
+  <Wrapper>
+    <Vote>
+      <Star
+        src={star}
+        alt=""
+      />
+    </Vote>
+    <Vote
+      average
+    >
+      7,7
+    </Vote>
+    <Vote
+      count
+    >
+      35 votes
+    </Vote>
+  </Wrapper>
+);
