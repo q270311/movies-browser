f924e6528f83aab7aebc7335e487829775209bbb
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-19 01:23:32 +0100

    Visual improvements

--- src/common/MovieTile/AdditionalInfo/index.js
+++ src/common/MovieTile/AdditionalInfo/index.js
@@ -3,12 +3,20 @@ import { Wrapper, Paragraph, Attribute, Value } from "./styled";
 export const AdditionalInfo = () => (
   <Wrapper>
     <Paragraph>
-      <Attribute>Production:&nbsp;</Attribute>
-      <Value>China USA</Value>
+      <Attribute>
+        Production:&nbsp;
+      </Attribute>
+      <Value>
+        China USA
+      </Value>
     </Paragraph>
     <Paragraph>
-      <Attribute>Release date:&nbsp;</Attribute>
-      <Value>24.10.2022</Value>
+      <Attribute>
+        Release date:&nbsp;
+      </Attribute>
+      <Value>
+        24.10.2022
+      </Value>
     </Paragraph>
   </Wrapper>
 );
