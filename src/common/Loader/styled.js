40a6c352ee427b1ae598a803e83aeb35155ab138
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-20 12:34:31 +0100

    animation added

--- src/common/Loader/styled.js
+++ src/common/Loader/styled.js
@@ -4,4 +4,10 @@ import { ReactComponent as LoaderIcon } from "./loaderIcon.svg";
 export const StyledIcon = styled(LoaderIcon)`
   width: 91px;
   animation: rotate 1s linear infinite;
+  
+  @keyframes rotate {
+    100% {
+      transform: rotate(360deg);
+    }
+  }
 `;
