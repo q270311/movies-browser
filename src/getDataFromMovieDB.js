185cfd6e56844f265c4be5e2741e4474b6fcd86f
Michał Bałdyga <q270311@gmail.com>
2023-02-20 22:17:07 +0100

    searching for a specific page of videos

--- src/getDataFromMovieDB.js
+++ src/getDataFromMovieDB.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const getMoviePopular = async () => {
-    const response = await axios.get("https://api.themoviedb.org/3/movie/popular?api_key=0a7a30a768304b21322288db13dcdb24&language=en-US&page=1");
+export const getMoviePopular = async ({ page }) => {
+    const response = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=0a7a30a768304b21322288db13dcdb24&language=en-US&page=${page}`);
     if (!response.ok) {
         new Error(response.statusText);
     }
