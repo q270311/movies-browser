21302f5bbc53d95989c60438fe47b6dbb08a09f7
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-20 12:34:12 +0100

    Styles added

--- src/common/Loader/styled.js
+++ src/common/Loader/styled.js
@@ -1,2 +1,7 @@
 import styled from "styled-components";
-import { ReactComponent as LoaderIcon } from "./loaderIcon.svg"
+import { ReactComponent as LoaderIcon } from "./loaderIcon.svg";
+
+export const StyledIcon = styled(LoaderIcon)`
+  width: 91px;
+  animation: rotate 1s linear infinite;
+`;
