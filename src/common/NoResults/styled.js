c2f68fe9a33890d8f74ba5c65d1557f98c641dd0
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-20 14:34:44 +0100

    animate added

--- src/common/NoResults/styled.js
+++ src/common/NoResults/styled.js
@@ -11,7 +11,21 @@ export const Wrapper = styled.div`
 `;
 
 export const StyledIcon = styled(NoResultsIcon)`
+  
   @media (max-width: ${({ theme }) => theme.breakpoint.mobileDevice}px) {
     transform: scale(0.5);
+  }
+
+  .animate {
+    animation: dasharray 2s linear infinite;
+
+    @keyframes dasharray { 
+      from {
+        stroke-dashoffset: 70;
+      }
+      to {
+        stroke-dashoffset: 0;
+      }
+    }
   }
 `;
