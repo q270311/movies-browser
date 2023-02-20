da02054c1a3d930c0b83d05604e3ae7917cba575
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-20 12:51:20 +0100

    Wrapper added

--- src/common/Loader/styled.js
+++ src/common/Loader/styled.js
@@ -1,6 +1,15 @@
 import styled from "styled-components";
 import { ReactComponent as LoaderIcon } from "./loaderIcon.svg";
 
+export const Wrapper = styled.div`
+  margin: 120px auto 0 auto;
+  text-align: center;
+  
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileDevice}px) {
+    margin-top: 24px;
+  }
+`;
+
 export const StyledIcon = styled(LoaderIcon)`
   width: 91px;
   animation: rotate 1s linear infinite;
