a252bf2c075a549976bc2edad5df0bd681c103c1
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-20 13:22:39 +0100

    Wrapper added

--- src/common/NoResults/styled.js
+++ src/common/NoResults/styled.js
@@ -1,1 +1,10 @@
 import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  margin: 40px auto 0 auto;
+  text-align: center;
+  
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileDevice}px) {
+    margin-top: 20px;
+  }
+`;
