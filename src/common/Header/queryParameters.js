c9183b2d4656608f8e0c3b5953301b20995a9de8
Joanna Panimasz <pani.joanna93@gmail.com>
2023-02-22 13:53:01 +0100

    create useReplaceQueryParameter hook

--- src/common/Header/queryParameters.js
+++ src/common/Header/queryParameters.js
@@ -1,7 +1,25 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export const useQueryParameter = (key) => {
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   return searchParams.get(key);
+};
+
+export const useReplaceQueryParameter = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  return ({ key, value }) => {
+    const searchParams = new URLSearchParams(location.search);
+
+    if (value === undefined) {
+      searchParams.delete(key);
+    } else {
+      searchParams.set(key, value);
+    }
+
+    const newSearch = searchParams.toString();
+    navigate(`${location.pathname}?${newSearch}`);
+  };
 };
