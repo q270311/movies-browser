bb58098cd54b544e27827384c998a1a0c5bc5a51
Joanna Panimasz <pani.joanna93@gmail.com>
2023-02-22 13:36:39 +0100

    create useQueryParameter hook

---
+++ src/common/Header/queryParameters.js
@@ -0,0 +1,7 @@
+import { useLocation } from "react-router-dom";
+
+export const useQueryParameter = (key) => {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  return searchParams.get(key);
+};
