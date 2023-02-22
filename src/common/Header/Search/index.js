8dd0eba9f6de31790cc3882fe28066340f42f86d
Joanna Panimasz <pani.joanna93@gmail.com>
2023-02-22 13:59:14 +0100

    create onInputChange in Search

--- src/common/Header/Search/index.js
+++ src/common/Header/Search/index.js
@@ -1,7 +1,22 @@
 import { SearchIcon, SearchInput, SearchWrapper } from "./styled";
 import search from "./search.svg";
+import {
+  useQueryParameter,
+  useReplaceQueryParameter,
+} from "../queryParameters";
+import searchQueryParamName from "../searchQueryParamName.js";
 
 const Search = () => {
+  const query = useQueryParameter(searchQueryParamName);
+  const replaceQueryParameter = useReplaceQueryParameter();
+
+  const onInputChange = ({ target }) => {
+    replaceQueryParameter({
+      key: searchQueryParamName,
+      value: target.value.trim() !== "" ? target.value : undefined,
+    });
+  };
+
   return (
     <SearchWrapper>
       <SearchIcon src={search} />
