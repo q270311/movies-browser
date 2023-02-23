8a2c024eec25f194da7a6a2341b3fdae9e0ed3fb
Michał Bałdyga <q270311@gmail.com>
2023-02-23 22:01:15 +0100

    change static location to path from router.js

--- src/common/Header/Navigation/index.js
+++ src/common/Header/Navigation/index.js
@@ -1,14 +1,15 @@
 import { StyledNavigation, NavList, NavItems } from "./styled";
 import { NavigationLink } from "../styled";
+import {toMoviesList, toPeopleList} from '../../../routes';
 
 const Navigation = () => {
   return (
     <StyledNavigation>
       <NavList>
-        <NavigationLink to="/movies-list">
+        <NavigationLink to={toMoviesList()}>
           <NavItems primary>Movies</NavItems>
         </NavigationLink>
-        <NavigationLink to="/people-list">
+        <NavigationLink to={toPeopleList()}>
           <NavItems>People</NavItems>
         </NavigationLink>
       </NavList>
