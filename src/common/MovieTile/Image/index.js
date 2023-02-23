10bce478aa2e6e48cee012f1ffef01b898c723e8
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-23 18:43:15 +0100

    noPoster added

--- src/common/MovieTile/Image/index.js
+++ src/common/MovieTile/Image/index.js
@@ -1,4 +1,5 @@
 import { Poster, ImageWrapper } from "./styled";
+import noPoster from "./noPoster.svg";
 
 export const Image = ({ posterPath }) => {
   const secureBaseUrl = "https://image.tmdb.org/t/p/w300";
