a5a77f74a1a5337ee05ee0261d744ec0fd9f7ba9
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-23 11:04:12 +0100

    Subtitle added

--- src/common/MainWrapper/styled.js
+++ src/common/MainWrapper/styled.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const PageWrapper = styled.div`
   max-width: 1400px;
+  margin: 0 auto;
   padding: 0 16px;
 `;
 
@@ -10,4 +11,11 @@ export const Title = styled.h1`
   font-weight: 600;
   margin-top: 56px;
   margin-bottom: 0;
+`;
+
+export const Subtitle = styled.h2`
+  font-size: 36px;
+  font-weight: 600;
+  margin-top: 64px;
+  margin-bottom: 32px;
 `;
