2281dbad5e7944ef27abfac3bfb73b9c5e113171
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-23 10:47:56 +0100

    Title styles added

--- src/common/MainWrapper/styled.js
+++ src/common/MainWrapper/styled.js
@@ -4,3 +4,10 @@ export const PageWrapper = styled.div`
   max-width: 1400px;
   padding: 0 16px;
 `;
+
+export const Title = styled.h1`
+  font-size: 36px;
+  font-weight: 600;
+  margin-top: 56px;
+  margin-bottom: 0;
+`;
