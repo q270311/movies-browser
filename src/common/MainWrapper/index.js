e5487db6095f08b1466a0f0d7ebd2fde2eb53d91
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-23 10:44:14 +0100

    Header added

--- src/common/MainWrapper/index.js
+++ src/common/MainWrapper/index.js
@@ -1,7 +1,9 @@
 import { PageWrapper } from "./styled";
+import Header from "../Header";
 
 export const MainWrapper = ({ children }) => (
   <>
+    <Header />
     <PageWrapper>
       {children}
     </PageWrapper>
