8250aa98ae178d4c79a252159a1a844aa9da391b
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-23 10:48:10 +0100

    Title added

--- src/common/MainWrapper/index.js
+++ src/common/MainWrapper/index.js
@@ -1,10 +1,11 @@
-import { PageWrapper } from "./styled";
+import { PageWrapper, Title } from "./styled";
 import Header from "../Header";
 
 export const MainWrapper = ({ children }) => (
   <>
     <Header />
     <PageWrapper>
+      <Title>Popular movies</Title>
       {children}
     </PageWrapper>
   </>
