56c768998666804c81b3e55d2fa247abed5cca4e
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-24 11:05:25 +0100

    MainWrapper added

--- src/common/Error/index.js
+++ src/common/Error/index.js
@@ -1,21 +1,26 @@
 import { Link } from "react-router-dom";
 import { Wrapper, ErrorIcon, Title, Subtitle, Button } from "./styled";
+import { MainWrapper } from "../MainWrapper";
 
 export const Error = () => {
   return (
-    <Wrapper>
-      <ErrorIcon />
-      <Title>
-        Ooops! Something went wrong...
-      </Title>
-      <Subtitle>
-        Please check your network connection and try again
-      </Subtitle>
-      <Link to="/">
-        <Button>
-          Back to home page
-        </Button>
-      </Link>
-    </Wrapper >
+    <MainWrapper
+      content={
+        <Wrapper>
+          <ErrorIcon />
+          <Title>
+            Ooops! Something went wrong...
+          </Title>
+          <Subtitle>
+            Please check your network connection and try again
+          </Subtitle>
+          <Link to="/">
+            <Button>
+              Back to home page
+            </Button>
+          </Link>
+        </Wrapper >
+      }
+    />
   );
 };
