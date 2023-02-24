44440c87d50b5df1b522d01f552940c2bfde8b4d
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-24 10:58:32 +0100

    Wrapper margin changed

--- src/common/NoResults/styled.js
+++ src/common/NoResults/styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { ReactComponent as NoResultsIcon } from "./noResultsIcon.svg"
 
 export const Wrapper = styled.div`
-  margin: 40px auto 0 auto;
+  margin: 150px auto 0 auto;
   text-align: center;
   
   @media (max-width: ${({ theme }) => theme.breakpoint.mobileDevice}px) {
