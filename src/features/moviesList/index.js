2077eb5eaf3fda279b4a2d1068654026569edd45
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-24 11:18:02 +0100

    status loading/error added

--- src/features/moviesList/index.js
+++ src/features/moviesList/index.js
@@ -3,34 +3,34 @@ import { Wrapper } from "./styled";
 import { Pagination } from "../../common/Pagination";
 import { MovieTile } from "../../common/MovieTile"
 import { selectData, selectStatus } from '../movieDatabaseSlice';
+import { Loader } from "../../common/Loader";
+import { Error } from "../../common/Error";
 
 const MoviesList = () => {
     const popularMovies = useSelector(selectData);
     const status = useSelector(selectStatus);
 
-    if (status !== "success") {
-        return (
-            <p>{status}</p>
-        );
-    }
     return (
-        <>
-            <Wrapper>
-                {popularMovies.map(movie => (
-                    <MovieTile
-                        key={movie.id}
-                        posterPath={movie.poster_path}
-                        title={movie.title}
-                        year={(movie.release_date || "").substring(0, 4)}
-                        genres={movie.genre_ids}
-                        voteAverage={movie.vote_average}
-                        voteCount={movie.vote_count}
-                    />
-                ))}
-            </Wrapper>
-            <Pagination />
-        </>
-
+        status === "loading" ?
+            <Loader /> :
+            status === "error" ?
+                <Error /> :
+                <>
+                    <Wrapper>
+                        {popularMovies.map(movie => (
+                            <MovieTile
+                                key={movie.id}
+                                posterPath={movie.poster_path}
+                                title={movie.title}
+                                year={(movie.release_date || "").substring(0, 4)}
+                                genres={movie.genre_ids}
+                                voteAverage={movie.vote_average}
+                                voteCount={movie.vote_count}
+                            />
+                        ))}
+                    </Wrapper>
+                    <Pagination />
+                </>
     );
 };
 
