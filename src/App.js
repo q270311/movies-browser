fdef5abe96bc7cbc638b9fbd07bc8ff5ae7d84df
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-26 22:20:28 +0100

    Routes bug fixed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { toMovieDetails, toMoviesList, toPeopleList, toPersonDetails } from "./routes";
 import MoviesList from "./features/moviesList";
 
 function App() {
   return (
     <>
-      <Routes>
-          <Route path="/" element={<MoviesList />} />
-          <Route path="/movies-browser" element={<MoviesList />} />       
+      <Routes>    
           <Route path={toMoviesList()} element={<MoviesList />} />
           <Route path={toPeopleList()} element="{<PeopleList />}" />
           <Route path={toMovieDetails()} element="{<MovieDetails />}" />
-          <Route path={toPersonDetails()} element="{<PersonDetails />}" />                   
+          <Route path={toPersonDetails()} element="{<PersonDetails />}" />      
+          <Route path="/" element={<Navigate to={toMoviesList()} />} />                    
       </Routes>
     </>
   );
