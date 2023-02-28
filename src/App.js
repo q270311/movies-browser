973fc933ce110c71def2e87810498c98c28dede8
Michał Bałdyga <q270311@gmail.com>
2023-02-28 13:56:15 +0100

    PeopleList page activated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { toMovieDetails, toMoviesList, toPeopleList, toPersonDetails } from "./routes";
 import MoviesList from "./features/moviesList";
+import PeopleList from "./features/peopleList";
 
 function App() {
   return (
     <>
       <Routes>    
           <Route path={toMoviesList()} element={<MoviesList />} />
-          <Route path={toPeopleList()} element="{<PeopleList />}" />
+          <Route path={toPeopleList()} element={<PeopleList />} />
           <Route path={toMovieDetails()} element="{<MovieDetails />}" />
           <Route path={toPersonDetails()} element="{<PersonDetails />}" />      
           <Route path="/" element={<Navigate to={toMoviesList()} />} />                    
