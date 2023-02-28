312ed1738036334dc208d3ad4ed377ee2f99f4c3
Michał Bałdyga <q270311@gmail.com>
2023-02-28 13:57:42 +0100

    peopleListSaga created

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,8 +1,10 @@
 import { all } from "redux-saga/effects";
 import { watchFetchMoviePopular } from './features/movieListSaga';
+import { watchFetchPersonPopular } from './features/peopleListSaga';
 
 export default function* rootSaga() {
     yield all([
         watchFetchMoviePopular(),
+        watchFetchPersonPopular(),
     ]);
 }
