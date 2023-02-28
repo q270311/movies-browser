c15a87ca410d8d991b4788c1ab015b4fa727095b
Michał Bałdyga <q270311@gmail.com>
2023-02-28 12:02:14 +0100

    rename file movieDatabaseSaga to movieListSaga

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,5 +1,5 @@
 import { all } from "redux-saga/effects";
-import { watchFetchMoviePopular } from './features/movieDatabaseSaga';
+import { watchFetchMoviePopular } from './features/movieListSaga';
 
 export default function* rootSaga() {
     yield all([
