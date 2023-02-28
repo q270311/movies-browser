4bc92297a0b354d412ab3657bb7ebd08beb637a7
Michał Bałdyga <q270311@gmail.com>
2023-02-28 14:00:43 +0100

    path to poster image added

--- src/common/PersonTile/index.js
+++ src/common/PersonTile/index.js
@@ -1,11 +1,19 @@
 import { PersonTileContainer, PersonImage, PersonHeading, PersonLink } from "./styled";
 
-const PersonTile = () => {
+const PersonTile = ({name, posterPath}) => {
+  const secureBaseUrl = "https://image.tmdb.org/t/p/w185";
+
   return (
     <PersonTileContainer>
       <PersonLink to="/">
-        <PersonImage />
-        <PersonHeading></PersonHeading>
+        <PersonImage 
+          src={
+            posterPath ?
+              `${secureBaseUrl}${posterPath}` :
+              "brak zdjęcia"//`${noPoster}`
+          }
+          alt="poster" />
+        <PersonHeading>{name}</PersonHeading>
       </PersonLink>
     </PersonTileContainer>
   );
