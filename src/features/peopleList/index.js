ca82fb7d57fabc4e9138c234ae8be7b68bd6c678
Michał Bałdyga <q270311@gmail.com>
2023-02-28 14:02:56 +0100

    PeopleList component added

---
+++ src/features/peopleList/index.js
@@ -0,0 +1,50 @@
+import { useSelector, useDispatch } from "react-redux";
+import { Wrapper } from "./styled";
+import { selectPeople, selectStatus, selectPage, selectTotalPages, goToPage } from '../peopleListSlice';
+import Loader from "../../common/Loader";
+import { Error } from "../../common/Error";
+import { MainWrapper } from "../../common/MainWrapper";
+import { Pagination } from "../../common/Pagination";
+import PersonTile from "../../common/PersonTile";
+
+
+const PeopleList = () => {
+    const popularPeople = useSelector(selectPeople);
+    const status = useSelector(selectStatus);
+    const pageNumber = useSelector(selectPage);
+    const totalPages = useSelector(selectTotalPages);
+    const dispatch = useDispatch();
+
+    return (
+        status === "loading" ?
+            <Loader /> :
+            status === "error" ?
+                <Error /> :
+                <>
+                    <MainWrapper
+                        content={
+                            <Wrapper>
+                                {popularPeople.map(person => (
+                                    <PersonTile
+                                        key={person.id}
+                                        name={person.name}
+                                        posterPath={person.profile_path}
+                                    />
+                                ))}
+                            </Wrapper>
+                        }
+                        title={"Popular people"}
+                    />
+                    <Pagination
+                        pageNumber={pageNumber}
+                        totalPages={totalPages}
+                        goToFirstPage={() => dispatch(goToPage({ page: 1 }))}
+                        goToNextPage={() => dispatch(goToPage({ page: pageNumber + 1 }))}
+                        goToPreviousPage={() => dispatch(goToPage({ page: pageNumber - 1 }))}
+                        goToLastPage={() => dispatch(goToPage({ page: totalPages }))}
+                    />
+                </>
+    );
+};
+
+export default PeopleList;
