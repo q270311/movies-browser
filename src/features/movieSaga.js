896a759470c2aec1c8532a7dc0a1060bd441c082
Michał Bałdyga <q270311@gmail.com>
2023-03-01 21:29:24 +0100

    movieSaga created

---
+++ src/features/movieSaga.js
@@ -0,0 +1,19 @@
+import { call, put, takeLatest, select } from "redux-saga/effects";
+import { fetchDataSuccess, fetchDataError, getDetailsForMovie, selectMovieId} from "./movieSlice";
+export const getMovieCredits = async ({ movieID }) => {
+import { getMovieDetails, getMovieCredits } from "../getDataFromMovieDB";
+
+function* fetchMovieDetailsHandler() {
+    try {        
+        const movieId = yield select(selectMovieId);
+        const details = yield call(getMovieDetails, { page: movieId });
+        const credits = yield call(getMovieCredits, { page: movieId });
+        yield put(fetchDataSuccess({details, credits}));/////
+    } catch (error) {
+        yield put(fetchDataError());
+    }
+}
+
+export function* watchFetchMovieDetails() {
+    yield takeLatest(getDetailsForMovie.type, fetchMovieDetailsHandler);
+}
