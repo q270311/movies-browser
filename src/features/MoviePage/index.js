929a4f49f67d6cc1ad12b311ac43199e593b8ee5
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-03-01 19:50:43 +0100

    MovieDescriptionTile added

--- src/features/MoviePage/index.js
+++ src/features/MoviePage/index.js
@@ -2,23 +2,41 @@ import { MainWrapper } from "../../common/MainWrapper"
 import poster from "./backdrop.jpg";
 import { Backdrop, BackdropWrapper, PosterWrapper, Title } from "./styled";
 import { BackdropVotes } from "../../common/Tiles/Votes";
+import { MovieDescriptionTile } from "../../common/Tiles";
 
-export const MoviePage = () => (
-  <MainWrapper
-    backdrop={
-      <BackdropWrapper>
-        <Title>
-          Plane
-        </Title>
-        <PosterWrapper>
-          <Backdrop
-            src={poster}
+export const MoviePage = () => {
+
+  return (
+    <MainWrapper
+      backdrop={
+        <BackdropWrapper>
+          <Title>
+            Plane
+          </Title>
+          <PosterWrapper>
+            <Backdrop
+              src={poster}
+            />
+          </PosterWrapper>
+          <BackdropVotes
+            voteAverage={"5"}
+            voteCount={"100"}
           />
-        </PosterWrapper>
-        <BackdropVotes
+        </BackdropWrapper>
+      }
+      content={
+        <MovieDescriptionTile
+          //posterPath={posterPath}
+          title={"title"}
+          year={("year" || "").substring(0, 4)}
+          //genres={"genres"}
           voteAverage={"5"}
           voteCount={"100"}
+          description={"A young Chinese maiden disguises..."}
+          firstValue={"China USA"}
+          secondValue={"24.10.2022"}
         />
-      </BackdropWrapper>
-    } />
-);
+      }
+    />
+  )
+};
