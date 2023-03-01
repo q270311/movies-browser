33edf92ffd586dbaf505c7d16c8a9edd51f00ace
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-03-01 16:17:20 +0100

    Backdrop, Poster/BackdropWrapper styles added

--- src/features/MoviePage/styled.js
+++ src/features/MoviePage/styled.js
@@ -1,10 +1,19 @@
 import styled from "styled-components";
 
 export const Backdrop = styled.img`
+  width: 100%;
 `;
-
 export const PosterWrapper = styled.div`
+  display: flex;
+  margin: 0 auto;
+  width: 75%;
 `;
 
 export const BackdropWrapper = styled.div`
+  width: 100%;
+  min-width: 280px;
+  display: flex;
+  margin: 0 auto;
+  justify-content: center;
+  position: relative;
 `;
