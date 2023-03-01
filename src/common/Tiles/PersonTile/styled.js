8b53e1fb75cfb35435115ecca32a8dc46672cdae
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-03-01 10:00:59 +0100

    PersonTile padding, width, height changed

--- src/common/Tiles/PersonTile/styled.js
+++ src/common/Tiles/PersonTile/styled.js
@@ -5,10 +5,9 @@ export const PersonTileContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 10px 15px 65px 16px;
-  gap: 12px;
-  width: 208px;
-  height: 339px;
+  padding: 16px;
+  min-width: 136px;
+  height: auto;
   background: ${({ theme }) => theme.color.white};
   transition: 0.7s linear;
   filter: drop-shadow(0px 4px 12px rgba(186, 199, 213, 0.5));
