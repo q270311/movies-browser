2c20faeed5f2e36a0e8213380f4ee562b9dde154
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-03-01 16:32:15 +0100

    Title styles added

--- src/features/MoviePage/styled.js
+++ src/features/MoviePage/styled.js
@@ -11,7 +11,15 @@ export const BackdropWrapper = styled.div`
   z-index: -1;
 `;
 
-export const Title = styled.h1``;
+export const Title = styled.h1`
+  position: absolute;
+  color: white;
+  left: 15%;
+  bottom: 20%;
+  height: auto;
+  font-size: 64px;
+  font-weight: 600;
+`;
 
 export const PosterWrapper = styled.div`
   display: flex;
