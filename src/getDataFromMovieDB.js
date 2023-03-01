90b983a1f61af6bf55f47f0017bbc8689cf413af
Michał Bałdyga <q270311@gmail.com>
2023-03-01 21:28:07 +0100

    getMovieDetails and getMovieCredits function added

--- src/getDataFromMovieDB.js
+++ src/getDataFromMovieDB.js
@@ -10,10 +10,10 @@ export const getMoviePopular = async ({ page }) => {
 };
 export const getGenres = async () => {
     const response = await axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=0a7a30a768304b21322288db13dcdb24&language=en-US`);
-    if (!response.ok) {        
+    if (!response.ok) {
         new Error(response.statusText);
     }
-        
+
     return await response.data.genres;
 };
 
@@ -24,4 +24,20 @@ export const getPersonPopular = async ({ page }) => {
     }
 
     return await response.data;
+};
+export const getMovieDetails = async ({ movieID }) => {
+    const response = await axios.get(`https://api.themoviedb.org/3/movie/${movieID}?api_key=0a7a30a768304b21322288db13dcdb24&language=en-US`);
+    if (!response.ok) {
+        new Error(response.statusText);
+    }
+
+    return await response.data.genres;
+};
+export const getMovieCredits = async ({ movieID }) => {
+    const response = await axios.get(`https://api.themoviedb.org/3/movie/${movieID}/credits?api_key=0a7a30a768304b21322288db13dcdb24&language=en-US`);
+    if (!response.ok) {
+        new Error(response.statusText);
+    }
+
+    return await response.data.genres;
 };
