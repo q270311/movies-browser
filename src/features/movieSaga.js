ff56b59e7aa9b20e40b53cf2d67d71711de01595
Michał Bałdyga <q270311@gmail.com>
2023-03-01 23:16:24 +0100

    add path to movie details page

--- src/features/movieSaga.js
+++ src/features/movieSaga.js
@@ -1,14 +1,13 @@
 import { call, put, takeLatest, select } from "redux-saga/effects";
-import { fetchDataSuccess, fetchDataError, getDetailsForMovie, selectMovieId} from "./movieSlice";
-export const getMovieCredits = async ({ movieID }) => {
+import { fetchDataSuccess, fetchDataError, getDetailsForMovie, selectMovieId } from "./movieSlice";
 import { getMovieDetails, getMovieCredits } from "../getDataFromMovieDB";
 
 function* fetchMovieDetailsHandler() {
-    try {        
-        const movieId = yield select(selectMovieId);
-        const details = yield call(getMovieDetails, { page: movieId });
-        const credits = yield call(getMovieCredits, { page: movieId });
-        yield put(fetchDataSuccess({details, credits}));
+    try {
+        const Id = yield select(selectMovieId);
+        const details = yield call(getMovieDetails, { movieID: Id });
+        const credits = yield call(getMovieCredits, { movieID: Id });
+        yield put(fetchDataSuccess({ details, credits }));
     } catch (error) {
         yield put(fetchDataError());
     }
