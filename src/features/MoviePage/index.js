ffaf4ffbf913e76130370e28b8dcb72a007a2476
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-03-01 16:27:34 +0100

    Title added

--- src/features/MoviePage/index.js
+++ src/features/MoviePage/index.js
@@ -1,12 +1,13 @@
 import { MainWrapper } from "../../common/MainWrapper"
 import poster from "./backdrop.jpg";
-import { Backdrop, BackdropWrapper, PosterWrapper } from "./styled";
+import { Backdrop, BackdropWrapper, PosterWrapper, Title } from "./styled";
 import { BackdropVotes } from "../../common/Tiles/Votes";
 
 export const MoviePage = () => (
   <MainWrapper
     backdrop={
       <BackdropWrapper>
+        <Title></Title>
         <PosterWrapper>
           <Backdrop
             src={poster}
--- src/features/MoviePage/styled.js
+++ src/features/MoviePage/styled.js
@@ -1,16 +1,5 @@
 import styled from "styled-components";
 
-export const Backdrop = styled.img`
-  width: 100%;
-  z-index: -1;
-`;
-
-export const PosterWrapper = styled.div`
-  display: flex;
-  margin: 0 auto;
-  width: 75%;
-`;
-
 export const BackdropWrapper = styled.div`
   width: 100%;
   min-width: 280px;
@@ -19,5 +8,18 @@ export const BackdropWrapper = styled.div`
   justify-content: center;
   position: relative;
   background-color: black;
+  z-index: -1;
+`;
+
+export const Title = styled.h1``;
+
+export const PosterWrapper = styled.div`
+  display: flex;
+  margin: 0 auto;
+  width: 75%;
+`;
+
+export const Backdrop = styled.img`
+  width: 100%;
   z-index: -1;
 `;
