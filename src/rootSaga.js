390aafb4227eb032f1fd754fface5e3b99dae3e7
Michał Bałdyga <q270311@gmail.com>
2023-03-01 23:21:27 +0100

    add watchFetchMovieDetails function to rootSaga

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,10 +1,12 @@
 import { all } from "redux-saga/effects";
 import { watchFetchMoviePopular } from './features/movieListSaga';
 import { watchFetchPersonPopular } from './features/peopleListSaga';
+import { watchFetchMovieDetails } from './features/movieSaga'
 
 export default function* rootSaga() {
     yield all([
         watchFetchMoviePopular(),
         watchFetchPersonPopular(),
+        watchFetchMovieDetails(),
     ]);
 }
