09c1c43c4de502cb9585eed48a2611fb6d70b125
Michał Bałdyga <q270311@gmail.com>
2023-03-02 10:19:22 +0100

    fixt/change url to API

--- src/getDataFromMovieDB.js
+++ src/getDataFromMovieDB.js
@@ -4,7 +4,7 @@ const baseURL = "https://api.themoviedb.org/3";
 const apiKey = "0a7a30a768304b21322288db13dcdb24";
 
 export const getMoviePopular = async ({ page }) => {
-    const response = await axios.get(`${baseURL}/movie/popular?apiKey=${apiKey}&language=en-US&page=${page}`);
+    const response = await axios.get(`${baseURL}/movie/popular?api_key=${apiKey}&language=en-US&page=${page}`);
     if (!response.ok) {
         new Error(response.statusText);
     }
@@ -12,7 +12,7 @@ export const getMoviePopular = async ({ page }) => {
     return await response.data;
 };
 export const getGenres = async () => {
-    const response = await axios.get(`${baseURL}/genre/movie/list?apiKey=${apiKey}&language=en-US`);
+    const response = await axios.get(`${baseURL}/genre/movie/list?api_key=${apiKey}&language=en-US`);
     if (!response.ok) {
         new Error(response.statusText);
     }
@@ -21,7 +21,7 @@ export const getGenres = async () => {
 };
 
 export const getPersonPopular = async ({ page }) => {
-    const response = await axios.get(`${baseURL}/person/popular?apiKey=${apiKey}&language=en-US&page=${page}`);
+    const response = await axios.get(`${baseURL}/person/popular?api_key=${apiKey}&language=en-US&page=${page}`);
     if (!response.ok) {
         new Error(response.statusText);
     }
@@ -29,7 +29,7 @@ export const getPersonPopular = async ({ page }) => {
     return await response.data;
 };
 export const getMovieDetails = async ({ movieID }) => {
-    const response = await axios.get(`${baseURL}/movie/${movieID}?apiKey=${apiKey}&language=en-US`);
+    const response = await axios.get(`${baseURL}/movie/${movieID}?api_key=${apiKey}&language=en-US`);
     if (!response.ok) {
         new Error(response.statusText);
     }
@@ -37,7 +37,7 @@ export const getMovieDetails = async ({ movieID }) => {
     return await response.data;
 };
 export const getMovieCredits = async ({ movieID }) => {
-    const response = await axios.get(`${baseURL}/movie/${movieID}/credits?apiKey=${apiKey}&language=en-US`);
+    const response = await axios.get(`${baseURL}/movie/${movieID}/credits?api_key=${apiKey}&language=en-US`);
     if (!response.ok) {
         new Error(response.statusText);
     }
