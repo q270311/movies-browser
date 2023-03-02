61ce99eef4262f3ef30f6931885d7a3a5e73a717
Michał Bałdyga <q270311@gmail.com>
2023-03-02 21:26:02 +0100

    Genres component render Genre from parameter

--- src/common/Tiles/Genres/index.js
+++ src/common/Tiles/Genres/index.js
@@ -1,22 +1,14 @@
 import { Wrapper, Genre } from "./styled";
 import { nanoid } from "@reduxjs/toolkit";
-import { useSelector } from "react-redux";
-import { selectGenres } from '../../../features/movieListSlice';
-
 
 export const Genres = ({ genres }) => {
-  const genresDictionary = useSelector(selectGenres);
   return (
     <Wrapper>
-      {genres.map(genre => {
-        const words = genresDictionary.filter(word => word.id === genre);
-        return (
+      {genres.map(genre => (
           <Genre key={nanoid()}>
-            {words[0].name}
+            {genre}
           </Genre>
-        )
-      }
-      )}
+        ))}
     </Wrapper>
   )
 };
