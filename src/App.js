82c91dd2d96ccecaf3a11b11848565c9ac4a2399
Michał Bałdyga <q270311@gmail.com>
2023-03-02 10:37:04 +0100

    come back MovieDetail component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import { toMovieDetails, toMoviesList, toPeopleList, toPersonDetails } from "./routes";
 import MoviesList from "./features/moviesList";
 import PeopleList from "./features/peopleList";
-//import MovieDetails  from './features/MovieDetails';
+import MovieDetails  from './features/movieDetails';
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <Routes>    
           <Route path={toMoviesList()} element={<MoviesList />} />
           <Route path={toPeopleList()} element={<PeopleList />} />
-          <Route path={toMovieDetails()} element={"<MovieDetails />"} />
+          <Route path={toMovieDetails()} element={<MovieDetails />} />
           <Route path={toPersonDetails()} element="{<PersonDetails />}" />      
           <Route path="/" element={<Navigate to={toMoviesList()} />} />                    
       </Routes>
