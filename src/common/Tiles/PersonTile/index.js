f61ca93443e2671b6e183eb222a5136a5d3a03c4
Michał Bałdyga <q270311@gmail.com>
2023-03-02 21:06:44 +0100

    add path url to person link

--- src/common/Tiles/PersonTile/index.js
+++ src/common/Tiles/PersonTile/index.js
@@ -1,13 +1,13 @@
 import { PersonTileContainer, PersonImage, PersonHeading, PersonSubtitle, PersonLink } from "./styled";
 import noPerson from "../Image/noPerson.svg"
 
-const PersonTile = ({ name, posterPath, subtitle }) => {
+const PersonTile = ({ id, name, posterPath, subtitle }) => {
   const secureBaseUrl = "https://image.tmdb.org/t/p/w185";
 
   return (
     <PersonTileContainer>
       <PersonLink
-        to="/"
+        to={`/person/${id}`}
       >
         <PersonImage
           src={
