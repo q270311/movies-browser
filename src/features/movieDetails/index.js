7fa2a14f9ab0cf1f6d75407c46e0a8eeb39c1127
Michał Bałdyga <q270311@gmail.com>
2023-03-02 09:44:12 +0100

    MovieDetails connected with store

--- src/features/movieDetails/index.js
+++ src/features/movieDetails/index.js
@@ -2,6 +2,12 @@ import { useParams } from "react-router-dom";
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getDetailsForMovie, selectDetails, selectCast, selectCrew } from '../movieSlice';
+import { MainWrapper } from "../../common/MainWrapper";
+import poster from "./backdrop.jpg";
+import { Backdrop, BackdropWrapper, PosterWrapper, Title, Wrapper } from "./styled";
+import { BackdropVotes } from "../../common/Tiles/Votes";
+import { MovieDescriptionTile } from "../../common/Tiles";
+import PersonTile from "../../common/Tiles/PersonTile";
 
 const MovieDetails = () => {
     const dispatch = useDispatch();
@@ -15,12 +21,55 @@ const MovieDetails = () => {
     const crew = useSelector(selectCrew);
 
     return (
-        <>
-            <p>details movie id: {id}</p>
-            <p>title: {details.original_title}</p>
-            <p>cast: {cast.length}</p>
-            <p>crew: {crew.length}</p>
-        </>
+        <MainWrapper
+            backdrop={
+                <BackdropWrapper>
+                    <Title>
+                        {details.original_title}
+                    </Title>
+                    <PosterWrapper>
+                        <Backdrop
+                            src={poster}
+                        />
+                    </PosterWrapper>
+                    <BackdropVotes
+                        voteAverage={"5"}
+                        voteCount={"100"}
+                    />
+                </BackdropWrapper>
+            }
+            content={
+                <MovieDescriptionTile
+                    //posterPath={posterPath}
+                    title={"title"}
+                    year={("year" || "").substring(0, 4)}
+                    //genres={"genres"}
+                    voteAverage={"5"}
+                    voteCount={"100"}
+                    description={"A young Chinese maiden disguises..."}
+                    firstValue={"China USA"}
+                    secondValue={"24.10.2022"}
+                />
+            }
+            firstSubtitle={`Cast (${cast.length})`}
+            firstSection={
+                <Wrapper>
+                    <PersonTile
+                        name={"name"}
+                        subtitle={"subtitle"}
+                    />
+                </Wrapper>
+            }
+            secondSubtitle={`Crew (${crew.length})`}
+            secondSection={
+                <Wrapper>
+                    <PersonTile
+                        name={"name"}
+                        subtitle={"subtitle"}
+                    />
+                </Wrapper>
+            }
+        />
     )
 }
 
