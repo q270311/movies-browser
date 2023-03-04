cc48c3f8d5623501a3f63cdd1ed92966d6b91515
Michał Bałdyga <q270311@gmail.com>
2023-03-04 21:30:46 +0100

    add genres to personDetails sore

--- src/features/personSlice.js
+++ src/features/personSlice.js
@@ -5,6 +5,7 @@ const personSlice = createSlice({
     initialState: {
         personId: 0,
         details: [],
+        genres: [], 
         cast: [],
         crew: [],
         status: "loading"
@@ -13,8 +14,9 @@ const personSlice = createSlice({
         fetchDataSuccess: (state, { payload }) => {
             state.status = "loading";
             state.details = payload.details;
-            state.cast = payload.credits.cast;
-            state.crew = payload.credits.crew;
+            state.genres = payload.genres;
+            state.cast = payload.credits.cast.filter(({ title }) => title);
+            state.crew = payload.credits.crew.filter(({ title }) => title);
             state.status = "success";
         },
         fetchDataError: (state) => {
@@ -34,6 +36,7 @@ export const {
 export const selectPersonState = (state) => state.personDetails;
 export const selectPersonId = (state) => selectPersonState(state).personId;
 export const selectDetails = (state) => selectPersonState(state).details;
+export const selectGenres = (state) => selectPersonState(state).genres;
 export const selectCrew = (state) => selectPersonState(state).crew;
 export const selectCast = (state) => selectPersonState(state).cast;
 export const selectStatus = (state) => selectPersonState(state).status;
