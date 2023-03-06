30ecf3b29730aa795738bea4d28dda24521b11ed
Michał Bałdyga <q270311@gmail.com>
2023-03-06 16:22:10 +0100

    Genres in MovieTile were over Tile

--- src/features/moviesList/styled.js
+++ src/features/moviesList/styled.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 export const Wrapper = styled.div`  
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(324px, 1fr));
+  grid-template-rows: auto;
   gap: 24px;
   margin-top: 24px;
 
