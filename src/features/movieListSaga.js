81ee9b5fa265f58d225a00eb4b6702efbcfeeedc
Michał Bałdyga <q270311@gmail.com>
2023-03-06 16:26:24 +0100

    get moviePopular data depending on the query

--- src/features/movieListSaga.js
+++ src/features/movieListSaga.js
@@ -1,13 +1,19 @@
 import { call, put, takeLatest, select } from "redux-saga/effects";
-import { fetchDataSuccess, fetchDataError, goToPage, selectPage } from "./movieListSlice";
-import { getGenres, getMoviePopular } from "../getDataFromMovieDB";
+import { fetchDataSuccess, fetchDataError, goToPage, selectPage, selectQuery } from "./movieListSlice";
+import { getGenres, getMoviePopular, searchMovie } from "../getDataFromMovieDB";
 
 function* fetchMoviePopularHandler() {
     try {
         const genres = yield call(getGenres);
         const page = yield select(selectPage);
-        const data = yield call(getMoviePopular, { page: page });
-        yield put(fetchDataSuccess({data, genres}));
+        const query = yield select(selectQuery);
+        let data;
+        if (query !== '') {
+            data = yield call(searchMovie, { page: page, query: query });
+        } else {
+            data = yield call(getMoviePopular, { page: page });
+        }
+        yield put(fetchDataSuccess({ data, genres }));
     } catch (error) {
         yield put(fetchDataError());
     }
