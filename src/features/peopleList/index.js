1d0b28582980cb16e13df3edc1472a553c0e200d
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-03-07 22:50:04 +0100

    NoResults added

--- src/features/peopleList/index.js
+++ src/features/peopleList/index.js
@@ -1,5 +1,5 @@
 import { useSelector, useDispatch } from "react-redux";
-import {useEffect} from "react";
+import { useEffect } from "react";
 import { Wrapper } from "./styled";
 import { selectPeople, selectStatus, selectPage, selectTotalPages, goToPage, selectTotalResults, setQuery } from '../peopleListSlice';
 import searchQueryParamName from "../searchQueryParamName";
@@ -9,6 +9,7 @@ import { Error } from "../../common/Error";
 import { MainWrapper } from "../../common/MainWrapper";
 import { Pagination } from "../../common/Pagination";
 import PersonTile from "../../common/Tiles/PersonTile";
+import { NoResults } from "../../common/NoResults";
 
 
 const PeopleList = () => {
@@ -31,32 +32,36 @@ const PeopleList = () => {
             status === "error" ?
                 <Error /> :
                 <>
-                    <MainWrapper
-                        content={
-                            <Wrapper>
-                                {popularPeople.map(person => (
-                                    <PersonTile
-                                        key={person.id}
-                                        id={person.id}
-                                        name={person.name}
-                                        posterPath={person.profile_path}
-                                    />
-                                ))}
-                            </Wrapper>
-                        }
-                        title={
-                            query ? `Search results for "${query}" (${totalResults})` :
-                            `Popular people`
-                        }
-                    />
-                    <Pagination
-                        pageNumber={pageNumber}
-                        totalPages={totalPages}
-                        goToFirstPage={() => dispatch(goToPage({ page: 1 }))}
-                        goToNextPage={() => dispatch(goToPage({ page: pageNumber + 1 }))}
-                        goToPreviousPage={() => dispatch(goToPage({ page: pageNumber - 1 }))}
-                        goToLastPage={() => dispatch(goToPage({ page: totalPages }))}
-                    />
+                    {totalResults === 0 ? <NoResults query={query} /> :
+                        (<>
+                            <MainWrapper
+                                content={
+                                    <Wrapper>
+                                        {popularPeople.map(person => (
+                                            <PersonTile
+                                                key={person.id}
+                                                id={person.id}
+                                                name={person.name}
+                                                posterPath={person.profile_path}
+                                            />
+                                        ))}
+                                    </Wrapper>
+                                }
+                                title={
+                                    query ? `Search results for "${query}" (${totalResults})` :
+                                        `Popular people`
+                                }
+                            />
+                            <Pagination
+                                pageNumber={pageNumber}
+                                totalPages={totalPages}
+                                goToFirstPage={() => dispatch(goToPage({ page: 1 }))}
+                                goToNextPage={() => dispatch(goToPage({ page: pageNumber + 1 }))}
+                                goToPreviousPage={() => dispatch(goToPage({ page: pageNumber - 1 }))}
+                                goToLastPage={() => dispatch(goToPage({ page: totalPages }))}
+                            />
+                        </>
+                        )}
                 </>
     );
 };
